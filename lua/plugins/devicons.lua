8a3d99b3dab56ee1cf808116f6f747ee04f8ab61
Soumyadip1004 <soumyadipdutta1004@gmail.com>
2025-12-24 15:11:43 +0530

    update devicons

--- lua/plugins/devicons.lua
+++ lua/plugins/devicons.lua
@@ -12,12 +12,18 @@ return {
         color = "#FFFFFF",
         name = "Markdown",
       },
+      ["tsconfig.json"] = { icon = "", color = "#377CC8", name = "TypeScript" },
+      ["favicon.ico"] = { icon = "", color = "#FFFFFF", name = "Favicon" },
     },
     override_by_extension = {
       ["js"] = { icon = "", color = "#F5DF20", name = "JavaScript" },
+      ["d.js"] = { icon = "", color = "#F5DF20", name = "JavaScript" },
+      ["ts"] = { icon = "", color = "#377CC8", name = "TypeScript" },
+      ["d.ts"] = { icon = "", color = "#377CC8", name = "TypeScript" },
       ["mjs"] = { icon = "", color = "#F5DF20", name = "JavaScript" },
       ["cjs"] = { icon = "", color = "#F5DF20", name = "JavaScript" },
       ["jsx"] = { icon = "󰜈", color = "#F5DF20", name = "JavaScriptReact" },
+      ["tsx"] = { icon = "󰜈", color = "#377CC8", name = "TypeScriptReact" },
     },
   },
 }
